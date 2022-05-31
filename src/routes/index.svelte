e2247d649753e8c7e990cfa64b891b9c861b1218
EliRodrEs <60432604+EliRodrEs@users.noreply.github.com>
2022-05-31 17:14:12 +0200

    ADD popular movies

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+  import { onMount } from "svelte";
+
+  export async function load({fetch}){
+    const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=285031c3b0efaff15415b39facc434ec&language=en-US&page=1`)
+    const data = await res.json()
+    console.log(data)
+    if(res.ok){
+      return {
+        props: {popular: data.results}
+      }
+    }
+  }
+
+</script>
+
+<script>
+  import PopularMovies from "../components/PopularMovies.svelte";
+  export let popular;
+</script>
+
+<section>
+  <PopularMovies {popular}/>
+</section>
